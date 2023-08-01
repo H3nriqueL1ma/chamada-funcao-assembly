3a90efba08a8d038108d68e6fc8b049f8f6c5e0a
Henrique Carlos <henriquecarlos361893@gmail.com>
2023-08-01 15:38:32 -0300

    Update program-function-call.asm

--- program-function-call.asm
+++ program-function-call.asm
@@ -1,25 +1,26 @@
 %include 'lib.inc'
 
-section .text
+section .text 
 
-global _start
+global _start 
 
 _start:
-    LEA         ESI, [BUFFER]; Load Effective Address
-    ADD         ESI, TAB
-    MOV         BYTE[ESI], LF
+    LEA         ESI, [BUFFER]  ; Carrega o endereço inicial do buffer 'BUFFER' no registrador ESI.
 
-    DEC         ESI
-    MOV         BYTE[ESI], 'C'
+    ADD         ESI, TAB       ; Adiciona o valor da constante 'TAB' ao endereço em ESI para apontar para a próxima posição no buffer.
+    MOV         BYTE[ESI], LF  ; Move o caractere de nova linha (LF) para a posição atual no buffer.
 
-    DEC         ESI
-    MOV         BYTE[ESI], 'B'
+    DEC         ESI            ; Decrementa o endereço em ESI para apontar para a posição anterior no buffer.
+    MOV         BYTE[ESI], 'C' ; Move o caractere 'C' para a posição atual no buffer.
 
-    DEC         ESI
-    MOV         BYTE[ESI], 'A'
-    
-    CALL        resultOutput
+    DEC         ESI            ; Decrementa o endereço em ESI para apontar para a posição anterior no buffer.
+    MOV         BYTE[ESI], 'B' ; Move o caractere 'B' para a posição atual no buffer.
 
-    MOV         EAX, SYS_EXIT
-    MOV         EBX, RET_EXIT
-    INT         SYS_CALL
+    DEC         ESI            ; Decrementa o endereço em ESI para apontar para a posição anterior no buffer.
+    MOV         BYTE[ESI], 'A' ; Move o caractere 'A' para a posição atual no buffer.
+
+    CALL        resultOutput   ; Chama a sub-rotina 'resultOutput' para exibir o conteúdo de 'BUFFER' na saída padrão.
+
+    MOV         EAX, SYS_EXIT  ; Move o número da chamada de sistema para encerrar o programa (SYS_EXIT) para o registrador EAX.
+    MOV         EBX, RET_EXIT  ; Move o código de retorno para a chamada de sistema SYS_EXIT (RET_EXIT) para o registrador EBX.
+    INT         SYS_CALL       ; Chama o sistema operacional para encerrar o programa.
